14923d4bd99e5bf159ec60dc0df2be26e6933daa
DESKTOP-7IR8EFG\alfin <final20.ap@gmail.com>
2017-09-08 16:12:44 +0700

    Database Migration : Post

---
+++ database/migrations/2017_09_08_083823_create_post_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->increments('id')->length(3);
+            $table->string('title',255);
+            $table->text('content');
+            $table->tinyInteger('status')->length(1)->unsigned();
+            $table->string('featured_images',255);
+            $table->tinyInteger('created_by')->length(3)->unsigned();
+            $table->tinyInteger('published_by')->length(3)->unsigned();
+            $table->string('tags',255)->nullable();
+            $table->tinyInteger('category_id')->unsigned();
+            $table->tinyInteger('is_hide')->length(1)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('flights');
+    }
+}
